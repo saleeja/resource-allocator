cfe94b27a12dbfe02cc458e0710096cfce08cbe0
saleeja <saleejas2@gmail.com>
2023-07-17 13:40:06 +0530

    students updated

--- resources/admin.py
+++ resources/admin.py
@@ -7,20 +7,36 @@ from .models import (
     Timings,
     Computer,
     Rooms,
+    Student,
 )
 
 # Register your models here.
 
+admin.site.register(Course)
 
-class Adminnn(admin.ModelAdmin):
-    title = ["", "", ""]
+class BatchAdmin(admin.ModelAdmin):
+    list_display = ('Batch_name', 'Timeslots', 'Course_Name')
+admin.site.register(Batch,BatchAdmin)
 
-
-admin.site.register(Course, Adminnn)
-admin.site.register(Batch)
 admin.site.register(Comp_Brand)
-admin.site.register(Trainer)
-admin.site.register(Timings)
-admin.site.register(Computer)
-admin.site.register(Rooms)
-    
+
+class Traineradmin(admin.ModelAdmin):
+    list_display = ('Trainer_Name', 'Course')
+admin.site.register(Trainer, Traineradmin)
+
+class TimingAdmin(admin.ModelAdmin):
+    list_display = ('Start_time', 'End_time')
+admin.site.register(Timings, TimingAdmin)
+
+class ComputerAdmin(admin.ModelAdmin):
+    list_display = ('Brand', 'Type', 'Assigned_trainer', 'serial_number')
+admin.site.register(Computer, ComputerAdmin)
+
+class RoomsAdmin(admin.ModelAdmin):
+    list_display = ('Room_name', 'Room_type')
+admin.site.register(Rooms,RoomsAdmin)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('Std_name', 'Batch', 'Trainer', 'Laptop')
+admin.site.register(Student, StudentAdmin)
+
--- resources/models.py
+++ resources/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-ownership = ((0, "owned"), (1, "rented"))
-category = ((0, "Laptop"), (1, "Desktop"))
-room_type = ((0, "Classroom"), (1, "Conference Rooms"))
 
 
 class Course(models.Model):
@@ -18,18 +15,17 @@ class Course(models.Model):
 
 
 class Trainer(models.Model):
-    TrainerName = models.CharField(max_length=220)
+    Trainer_Name = models.CharField(max_length=220)
     Course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.TrainerName
+        return self.Trainer_Name
 
     class Meta:
         verbose_name_plural = "Trainers"
 
 
 class Timings(models.Model):
-
     Start_time = models.TimeField()
     End_time = models.TimeField()
 
@@ -38,19 +34,27 @@ class Timings(models.Model):
 
     def __str__(self):
         return f"{self.Start_time} - {self.End_time}"
-    
+
     class Meta:
         verbose_name_plural = "Timings"
 
 
 class Batch(models.Model):
-    Batch_name = models.CharField(max_length=220)
+    Batch_name = models.CharField(max_length=220, blank=True)
     Timeslots = models.ForeignKey(Timings, on_delete=models.CASCADE)
+    Course_Name = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.Batch_name
-    
 
+    def save(self, *args, **kwargs):
+        if not self.Batch_name:
+            # Generate the Batch_name based on the associated Timeslots
+            course_name = self.Course_Name.Course
+            start_time = self.Timeslots.Start_time
+            batch_name = f"{course_name}{start_time.strftime('%I%p')}Batch"
+            self.Batch_name = batch_name
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "Batches"
@@ -67,22 +71,47 @@ class Comp_Brand(models.Model):
 
 
 class Computer(models.Model):
-    Comp_code = models.CharField(max_length=50)
-    Category = models.IntegerField(choices=category, null=False)
+    ownership = ((0, "owned"), (1, "rented"))
+
+    # Comp_code = models.CharField(max_length=50)
+    # Category = models.IntegerField(choices=category, null=False)
     Brand = models.ForeignKey(Comp_Brand, on_delete=models.CASCADE)
     Type = models.IntegerField(choices=ownership, null=False)
     Assigned_trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    serial_number = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return self.Comp_code
+        return self.serial_number
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            # Generate new serial number only for new instances
+            last_serial_number = (
+                Computer.objects.order_by("-id")
+                .values_list("serial_number", flat=True)
+                .first()
+            )
+            if last_serial_number:
+                
+                last_number = int(last_serial_number[3:])
+                new_number = last_number + 1
+            else:
+                new_number = 1
+
+            self.serial_number = f"OTL{new_number}"
+
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "Computers"
 
 
 class Rooms(models.Model):
-    Room_name= models.CharField(max_length=100)
-    Room_type= models.IntegerField(choices=room_type, null=False)
+
+    room_type = ((0, "Classroom"), (1, "Conference Rooms"))
+
+    Room_name = models.CharField(max_length=100)
+    Room_type = models.IntegerField(choices=room_type, null=False)
 
     def __str__(self):
         return self.Room_name
@@ -91,9 +120,25 @@ class Rooms(models.Model):
         verbose_name_plural = "Rooms"
 
 
-# class Student(models.Model):
-#     Std_name=models.CharField(max_length=150)
-#     Course=models.ForeignKey(Course, on_delete=models.CASCADE)
-#     Batch=models.ForeignKey(Batch,on_delete=models.CASCADE)
-#     Timings=models.ForeignKey(Timings,on_delete=models.CASCADE)
-#     Trainer=models.ForeignKey(Trainer,on_delete=models.CASCADE)
+class Student(models.Model):
+
+    laptop = ((0, "Self"), (1, "OTS"))
+
+    Std_name = models.CharField(max_length=150)
+    Batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
+    Trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    Laptop = models.IntegerField(choices=laptop, null=False)
+    # Computer = models.ForeignKey(Computer, on_delete=models.CASCADE, blank=True)
+    temp = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.Std_name
+
+    def save(self, *args, **kwargs):
+        if self.Laptop == 1:
+            Ots_lap = Computer.serial_number
+            self.temp = Ots_lap
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Students"
